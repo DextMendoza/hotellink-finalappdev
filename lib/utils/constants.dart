01a096042f3b48d287d1c3434e59998f02e8f0e1
DextMendoza <148871320+DextMendoza@users.noreply.github.com>
2025-07-14 20:41:48 +0800

    Update constants.dart
    new static const, backgroundGradient for screen backgrounds

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -6,7 +6,6 @@ class Constants {
   static const Color secondaryColor = Colors.grey;
   static const Color backgroundColor = Color.fromARGB(243, 250, 250, 250);
   static const Color textColor = Color.fromARGB(255, 66, 66, 66);
-  
 
   // Fonts
   static const String fontFamily = 'Roboto';
@@ -25,8 +24,19 @@ class Constants {
   static const String payrollReportRoute = '/payroll-report';
   static const String profileRoute = '/profile';
 
-  //Variables
+  //Additional Constants
   static final emailRegex = RegExp(
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
   );
+  static const backgroundGradient = LinearGradient( //constant for gradient background
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color.fromARGB(255, 53, 155, 202),
+      Color.fromARGB(255, 49, 118, 150), 
+    ],
+  );
+  
+
+
 }
