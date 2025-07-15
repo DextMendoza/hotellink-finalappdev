96e0ddafaea7e1cd4d7bfeb385398fc6971ef87f
DextMendoza <148871320+DextMendoza@users.noreply.github.com>
2025-07-15 10:33:13 +0800

    dashboard UI update
    added background, and more intuitive buttons to access the main features of the app (employee management, attendance mgr, payroll report)

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -4,6 +4,7 @@ import 'package:final_project_in_appdev/screens/attendance_manager.dart';
 import 'package:final_project_in_appdev/screens/payroll_report.dart';
 import 'package:final_project_in_appdev/screens/profile_page.dart';
 import 'package:final_project_in_appdev/screens/login_screen.dart';
+import 'package:final_project_in_appdev/utils/constants.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -15,64 +16,160 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.person),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ProfilePage(),
-                ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: Constants.backgroundGradient,
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ProfilePage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        drawer: const NavigationDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              final isWide = constraints.maxWidth > 600;
+              final crossAxisCount = isWide ? 3 : 2;
+
+              return GridView.count(
+                crossAxisCount: crossAxisCount,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: 1,
+                shrinkWrap: true,
+                children: [
+                  _AnimatedTile(
+                    icon: Icons.people,
+                    label: 'Employee Management',
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const EmployeeManagement()),
+                    ),
+                  ),
+                  _AnimatedTile(
+                    icon: Icons.access_time,
+                    label: 'Attendance Manager',
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AttendanceManager()),
+                    ),
+                  ),
+                  _AnimatedTile(
+                    icon: Icons.receipt_long,
+                    label: 'Payroll Report',
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const PayrollReport()),
+                    ),
+                  ),
+                ],
               );
             },
           ),
-        ],
+        ),
       ),
-      drawer: const NavigationDrawer(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const EmployeeManagement(),
-                  ),
-                );
-              },
-              child: const Text('Employee Management'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const AttendanceManager(),
-                  ),
-                );
-              },
-              child: const Text('Attendance Manager'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const PayrollReport(),
-                  ),
-                );
-              },
-              child: const Text('Payroll Report'),
-            ),
-          ],
+    );
+  }
+}
+
+class _AnimatedTile extends StatefulWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const _AnimatedTile({
+    required this.icon,
+    required this.label,
+    required this.onTap,
+  });
+
+  @override
+  State<_AnimatedTile> createState() => _AnimatedTileState();
+}
+
+class _AnimatedTileState extends State<_AnimatedTile> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+      lowerBound: 0.95,
+      upperBound: 1.0,
+    );
+    _scaleAnimation = _controller.drive(Tween(begin: 1.0, end: 0.95));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(_) => _controller.reverse();
+  void _onTapUp(_) => _controller.forward();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: (details) {
+        _onTapUp(details);
+        widget.onTap();
+      },
+      onTapCancel: _controller.forward,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.9),
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 8,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(widget.icon, size: 48, color: Constants.primaryColor),
+              const SizedBox(height: 12),
+              Text(
+                widget.label,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Constants.textColor,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -90,7 +187,7 @@ class NavigationDrawer extends StatelessWidget {
         children: [
           const DrawerHeader(
             decoration: BoxDecoration(
-              color: Colors.blue,
+              color: Constants.primaryColor,
             ),
             child: Text(
               'Payroll Management',
@@ -99,9 +196,7 @@ class NavigationDrawer extends StatelessWidget {
           ),
           ListTile(
             title: const Text('Dashboard'),
-            onTap: () {
-              Navigator.pop(context);
-            },
+            onTap: () => Navigator.pop(context),
           ),
           ListTile(
             title: const Text('Employee Management'),
@@ -109,9 +204,7 @@ class NavigationDrawer extends StatelessWidget {
               Navigator.pop(context);
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => const EmployeeManagement(),
-                ),
+                MaterialPageRoute(builder: (context) => const EmployeeManagement()),
               );
             },
           ),
@@ -121,9 +214,7 @@ class NavigationDrawer extends StatelessWidget {
               Navigator.pop(context);
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => const AttendanceManager(),
-                ),
+                MaterialPageRoute(builder: (context) => const AttendanceManager()),
               );
             },
           ),
@@ -133,9 +224,7 @@ class NavigationDrawer extends StatelessWidget {
               Navigator.pop(context);
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => const PayrollReport(),
-                ),
+                MaterialPageRoute(builder: (context) => const PayrollReport()),
               );
             },
           ),
@@ -145,9 +234,7 @@ class NavigationDrawer extends StatelessWidget {
               Navigator.pop(context);
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(
-                  builder: (context) => const LoginScreen(),
-                ),
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
               );
             },
           ),
