a5a5db0ebd97e96dde7c0d5307e7ea2066bce317
DextMendoza <148871320+DextMendoza@users.noreply.github.com>
2025-07-16 20:17:01 +0800

    payroll_screen compatibility
    enables tojson and fromjson features

--- lib/models/payroll_record.dart
+++ lib/models/payroll_record.dart
@@ -10,4 +10,22 @@ class PayrollRecord {
     required this.month,
     required this.salary,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'employeeId': employeeId,
+      'month': month,
+      'salary': salary,
+    };
+  }
+
+  factory PayrollRecord.fromJson(Map<String, dynamic> json) {
+    return PayrollRecord(
+      id: json['id'],
+      employeeId: json['employeeId'],
+      month: json['month'],
+      salary: (json['salary'] as num).toDouble(),
+    );
+  }
 }
