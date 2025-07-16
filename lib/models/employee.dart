42cbf7459c81774e3db77ff65a4e8b07ab9a777c
DextMendoza <148871320+DextMendoza@users.noreply.github.com>
2025-07-16 08:36:20 +0800

    Update employee.dart

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -1,11 +1,13 @@
-class Employee {
-  String id;
-  String employeeId;
-  String name;
-  String email;
-  double salary;
+import 'dart:convert';
 
-  static List<Employee> allEmployees = [];
+class Employee {
+  final String id;
+  final String employeeId;
+  final String name;
+  final String email;
+  final double salary;
+
+  static var allEmployees;
 
   Employee({
     required this.id,
@@ -14,4 +16,28 @@ class Employee {
     required this.email,
     required this.salary,
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'employeeId': employeeId,
+        'name': name,
+        'email': email,
+        'salary': salary,
+      };
+
+  static Employee fromJson(Map<String, dynamic> json) => Employee(
+        id: json['id'],
+        employeeId: json['employeeId'],
+        name: json['name'],
+        email: json['email'],
+        salary: json['salary'],
+      );
+
+  static String encode(List<Employee> employees) =>
+      jsonEncode(employees.map((e) => e.toJson()).toList());
+
+  static List<Employee> decode(String jsonStr) =>
+      (jsonDecode(jsonStr) as List<dynamic>)
+          .map<Employee>((e) => Employee.fromJson(e))
+          .toList();
 }
