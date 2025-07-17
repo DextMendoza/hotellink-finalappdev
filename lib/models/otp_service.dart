bfe2a06602b6f24487543c1a6710cb85596b827e
DextMendoza <148871320+DextMendoza@users.noreply.github.com>
2025-07-17 15:51:22 +0800

    added otp_service.dart
    used to generate the otp used for signing up, contains the EmailJS API request as well

---
+++ lib/models/otp_service.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class OtpService {
+  static Future<String?> sendOtp(String email) async {
+    const serviceId = 'service_kces6fg';
+    const templateId = 'template_zh5zolj';
+    const userId = 'DjKZ32hhsQEjPrI3h'; // public key from EmailJS
+    final otp = _generateOtp();
+
+    final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+
+    final response = await http.post(
+      url,
+      headers: {
+        'origin': 'http://localhost',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'service_id': serviceId,
+        'template_id': templateId,
+        'user_id': userId,
+        'template_params': {
+          'to_email': email,
+          'otp': otp,
+        },
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return otp;
+    } else {
+      print('Failed to send OTP: ${response.body}');
+      return null;
+    }
+  }
+
+  static String _generateOtp() {
+    final random = DateTime.now().millisecondsSinceEpoch;
+    return (random % 1000000).toString().padLeft(6, '0');
+  }
+}
