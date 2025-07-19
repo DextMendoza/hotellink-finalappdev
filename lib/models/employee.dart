da7cb0c4d5465e662aefb0cfb0e3b643913de08f
Kyle-Geronimo <KyleGeronimo360@gmail.com>
2025-07-19 15:00:40 +0800

    Update employee.dart

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+// Employee Account
 class Employee {
   final String id;
   final String employeeId;
@@ -9,6 +10,7 @@ class Employee {
 
   static var allEmployees;
 
+  // Constructor for  an Employee
   Employee({
     required this.id,
     required this.employeeId,
@@ -17,25 +19,29 @@ class Employee {
     required this.salary,
   });
 
+  // Converts the Employee to JSON format
   Map<String, dynamic> toJson() => {
-        'id': id,
-        'employeeId': employeeId,
-        'name': name,
-        'email': email,
-        'salary': salary,
-      };
+    'id': id,
+    'employeeId': employeeId,
+    'name': name,
+    'email': email,
+    'salary': salary,
+  };
 
+  // Creates an Employee from JSON format
   static Employee fromJson(Map<String, dynamic> json) => Employee(
-        id: json['id'],
-        employeeId: json['employeeId'],
-        name: json['name'],
-        email: json['email'],
-        salary: json['salary'],
-      );
+    id: json['id'],
+    employeeId: json['employeeId'],
+    name: json['name'],
+    email: json['email'],
+    salary: json['salary'],
+  );
 
+  // Encodes a list of Employees to JSON string
   static String encode(List<Employee> employees) =>
       jsonEncode(employees.map((e) => e.toJson()).toList());
 
+  // Decodes the JSON string to a list of EMployees
   static List<Employee> decode(String jsonStr) =>
       (jsonDecode(jsonStr) as List<dynamic>)
           .map<Employee>((e) => Employee.fromJson(e))
