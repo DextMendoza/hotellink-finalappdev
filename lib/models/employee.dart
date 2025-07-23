67248327a3a71f5d1747316c382c5ba16300d07f
DextMendoza <148871320+DextMendoza@users.noreply.github.com>
2025-07-23 22:46:35 +0800

    Update employee.dart
    reduced line count, new parameter 'position'

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -1,49 +1,40 @@
 import 'dart:convert';
 
-// Employee Account
 class Employee {
-  final String id;
   final String employeeId;
   final String name;
   final String email;
-  final double salary;
+  final String position;
 
-  static var allEmployees;
-
-  // Constructor for  an Employee
   Employee({
-    required this.id,
     required this.employeeId,
     required this.name,
     required this.email,
-    required this.salary,
+    required this.position,
   });
 
-  // Converts the Employee to JSON format
+  factory Employee.fromJson(Map<String, dynamic> json) => Employee(
+        employeeId: json['employeeId'],
+        name: json['name'],
+        email: json['email'],
+        position: json['position'],
+      );
+
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'employeeId': employeeId,
-    'name': name,
-    'email': email,
-    'salary': salary,
-  };
+        'employeeId': employeeId,
+        'name': name,
+        'email': email,
+        'position': position,
+      };
 
-  // Creates an Employee from JSON format
-  static Employee fromJson(Map<String, dynamic> json) => Employee(
-    id: json['id'],
-    employeeId: json['employeeId'],
-    name: json['name'],
-    email: json['email'],
-    salary: json['salary'],
-  );
+  // Encoding list of employees to JSON string
+  static String encode(List<Employee> employees) => jsonEncode(
+        employees.map<Map<String, dynamic>>((e) => e.toJson()).toList(),
+      );
 
-  // Encodes a list of Employees to JSON string
-  static String encode(List<Employee> employees) =>
-      jsonEncode(employees.map((e) => e.toJson()).toList());
-
-  // Decodes the JSON string to a list of EMployees
-  static List<Employee> decode(String jsonStr) =>
-      (jsonDecode(jsonStr) as List<dynamic>)
+  // Decoding JSON string back to list of employees
+  static List<Employee> decode(String employees) =>
+      (jsonDecode(employees) as List<dynamic>)
           .map<Employee>((e) => Employee.fromJson(e))
           .toList();
 }
